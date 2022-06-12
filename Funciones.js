4a77ef52fc3e5a3bace72d9955aa8cd6dd3e9589
tomig <2105905@ucc.edu.ar>
2022-06-12 18:55:04 -0300

    Vidas y Tiempo funcionando

--- Funciones.js
+++ Funciones.js
@@ -12,7 +12,7 @@ var TableroNormal = [5, 1, 3, 2, 4, 7, 8, 9, 6, 6, 2, 8, 9, 5, 1, 4, 7, 3, 7, 9,
 var TableroNormalVacio = [0, 1, 0, 0, 4, 0, 0, 9, 6, 6, 0, 0, 9, 0, 1, 0, 7, 0, 0, 0, 4, 0, 0, 8, 2, 0, 1, 0, 7, 0, 8, 0, 0, 5, 0, 2, 2, 0, 5, 7, 0, 0, 0, 0, 8, 0, 8, 0, 0, 2, 0, 9, 3, 0, 0, 6, 0, 4, 0, 5, 0, 2, 0, 9, 0, 7, 0, 0, 2, 0, 8, 0, 0, 3, 0, 6, 0, 9, 0, 0, 5];
 var TableroDificil = [6, 4, 8, 1, 2, 5, 3, 9, 7, 7, 5, 3, 8, 9, 6, 1, 4, 2, 2, 1, 9, 7, 3, 4, 5, 8, 6, 9, 7, 2, 3, 1, 8, 4, 6, 5, 8, 6, 4, 5, 7, 2, 9, 3, 1, 5, 3, 1, 6, 4, 9, 2, 7, 8, 1, 9, 7, 2, 6, 3, 8, 5, 4, 3, 8, 6, 4, 5, 1, 7, 2, 9, 4, 2, 5, 9, 8, 7, 6, 1, 3];
 var TableroDificilVacio = [6, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 3, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 8, 0, 9, 0, 0, 0, 0, 0, 0, 0, 5, 0, 6, 0, 0, 0, 2, 0, 0, 0, 0, 0, 1, 0, 0, 9, 0, 0, 0, 0, 9, 0, 0, 0, 0, 8, 0, 0, 0, 8, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 1, 0];
-
+var Resultado;
 var vidas;
 vidas = localStorage.getItem("vidasLS");
 
@@ -24,7 +24,6 @@ function Dificultad(dificultad) {
 }
 
 function Vidas(vidas) {
-    alert(vidas);
     if (vidas == undefined) {
         vidas = 10;
     }
@@ -70,18 +69,21 @@ function CrearSudoku() {
 }
 
 function Tablero(i, num) {
-    var Terminar = 0, Resultado;
+    var Terminar = 0;
     var Dificultad;
     Dificultad = localStorage.getItem("dificultadLS");
+    
     i--;
 
     if (Dificultad == "facil") {
         TableroFacilVacio[i] = num;
         if (TableroFacil[i] != TableroFacilVacio[i]) {
+            document.getElementsByTagName("input").disabled = true;
             document.getElementsByClassName("celda")[i].classList.add("Incorrecto");
             setTimeout(function () {
                 document.getElementsByClassName("celda")[i].classList.remove("Incorrecto");
                 document.getElementsByClassName("celda")[i].value = "";
+                document.getElementsByTagName("input").disabled = false;
             }, 1000);
             vidas--;
         } else {
@@ -95,10 +97,12 @@ function Tablero(i, num) {
     if (Dificultad == "normal") {
         TableroNormalVacio[i] = num;
         if (TableroNormal[i] != TableroNormalVacio[i]) {
+            document.getElementsByTagName("input").disabled = true;
             document.getElementsByClassName("celda")[i].classList.add("Incorrecto");
             setTimeout(function () {
                 document.getElementsByClassName("celda")[i].classList.remove("Incorrecto");
                 document.getElementsByClassName("celda")[i].value = "";
+                document.getElementsByTagName("input").disabled = false;
             }, 1000);
             vidas--;
         } else {
@@ -112,10 +116,12 @@ function Tablero(i, num) {
     if (Dificultad == "dificil") {
         TableroDificilVacio[i] = num;
         if (TableroDificil[i] != TableroDificilVacio[i]) {
+            document.getElementsByTagName("input").disabled = true;
             document.getElementsByClassName("celda")[i].classList.add("Incorrecto");
             setTimeout(function () {
                 document.getElementsByClassName("celda")[i].classList.remove("Incorrecto");
                 document.getElementsByClassName("celda")[i].value = "";
+                document.getElementsByTagName("input").disabled = false;
             }, 1000);
             vidas--;
         } else {
@@ -133,71 +139,104 @@ function Tablero(i, num) {
         Terminar = 1;
         Resultado = "Perdio";
     }
-
-    for (let j = 0; j < 81; j++) {
-        if (Dificultad == "facil") {
-            if (TableroFacilVacio[i] == TableroFacil[i]) {
-                Terminar = 1;
-                Resultado = "Gano";
-            } else {
-                Terminar = 0;
-                j = 81;
+    else {
+        for (let j = 0; j < 81; j++) {
+            if (Dificultad == "facil") {
+                if (TableroFacilVacio[j] == TableroFacil[j]) {
+                    Terminar = 1;
+                    Resultado = "Gano";
+                } else {
+                    Terminar = 0;
+                    j = 81;
+                }
             }
-        }
-        if (Dificultad == "normal") {
-            if (TableroNormalVacio[i] == TableroNormal[i]) {
-                Terminar = 1;
-                Resultado = "Gano";
-            } else {
-                Terminar = 0;
-                j = 81;
+            if (Dificultad == "normal") {
+                if (TableroNormalVacio[j] == TableroNormal[j]) {
+                    Terminar = 1;
+                    Resultado = "Gano";
+                } else {
+                    Terminar = 0;
+                    j = 81;
+                }
             }
-        }
-        if (Dificultad == "dificil") {
-            if (TableroDificilVacio[i] == TableroDificilVacio[i]) {
-                Terminar = 1;
-                Resultado = "Gano";
-            } else {
-                Terminar = 0;
-                j = 81;
+            if (Dificultad == "dificil") {
+                if (TableroDificilVacio[j] == TableroDificilVacio[j]) {
+                    Terminar = 1;
+                    Resultado = "Gano";
+                } else {
+                    Terminar = 0;
+                    j = 81;
+                }
             }
         }
     }
 
     if (Terminar == 1) {
-        if (Resultado == "Gano") {
-            alert(Gano);
-        } else {
-            alert(Perdio);
-        }
+        endgame();
     }
 }
 
-var timer;
-timer = localStorage.getItem("timerLS");
-var timeRemaining;
-
-function startTimer(tiempo) {
-    if (timer == undefined) {
-        timer = 900;
+function Tiempo(tiempo) {
+    if (tiempo == undefined) {
+        tiempo = "15_min";
     }
     localStorage.setItem("timerLS", tiempo);
-    if (tiempo == "10_min") timeRemaining = 600; else if (tiempo == "15_min") timeRemaining = 900; else if (tiempo == "20_min") timeRemaining = 1200; else timeRemaining = 1500;
+}
 
-    //Empieza el temporizador
-    id("timer").textContent = timeConversion(timeRemaining);
-    //Actualizar el temporizador cada 1 segundo
-    timer = setInterval(function () {
-        timeRemaining--;
-        if (timeRemaining === 0) endgame();
-        id("timer").textContent = timeConversion(timeRemaining);
-    }, 1000)
+function starTimer() {
+    var timer;
+    timer = localStorage.getItem("timerLS");
+    var timeRemaining;
+    if (timer == "10_min") {
+        timeRemaining = 600;
+    }else if (timer == "15_min") {
+        timeRemaining = 900;
+    }else if (timer == "20_min") {
+        timeRemaining = 1200;
+    }else if (timer == "25_min"){
+        timeRemaining = 1500;
+    }
+    else {
+        timeRemaining = "ilimitado";
+    }
+    if (timeRemaining != "ilimitado") {
+       //Empieza el temporizador
+        document.getElementById("timer").innerHTML = timeConversion(timeRemaining);
+        //Actualizar el temporizador cada 1 segundo
+        timer = setInterval(function () {
+            if (timeRemaining == 0) {
+                Resultado = "Perdio"
+                endgame();
+                clearInterval(timer);
+            }
+            else {
+                timeRemaining --;
+                document.getElementById("timer").innerHTML = timeConversion(timeRemaining);
+            }
+        }, 1000) 
+    }
+    else {
+        document.getElementById("timer").innerHTML = "Tiempo | Ilimitado";
+    }
 }
 
 function timeConversion(time) {
     let minutes = Math.floor(time / 60);
-    if (minutes < 10) minutes = "0" + minutes;
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
     let seconds = time % 60;
-    if (seconds < 10) seconds = "0" + seconds;
-    return document.getElementById("timer").innerHTML = "Tiempo: " + minutes + ":" + seconds;
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    return document.getElementById("timer").innerHTML = "Tiempo | " + minutes + ":" + seconds;
+}
+
+function endgame() {
+    document.getElementsByTagName("input").disabled = true;
+    if (Resultado == "Gano") {
+        alert("Gano");
+    } else {
+        alert("Perdio");
+    }
 }
